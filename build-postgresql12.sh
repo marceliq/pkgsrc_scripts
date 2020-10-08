e57d70553d59fa9c1f1dbe68700cd818aeb762f5
marceliq <mhnilka@gmail.com>
2020-10-08 22:48:41 +0000

    added pgbouncer to postgresql build

--- build-postgresql12.sh
+++ build-postgresql12.sh
@@ -11,9 +11,9 @@ CVS_BRANCH="HEAD"
 #CVS_BRANCH="pkgsrc-2019Q4"
 #PKGSRC_URL="https://cdn.netbsd.org/pub/pkgsrc/current/pkgsrc.tar.gz"
 
-PKGSRC_MODULES="rb/postgresql12-server"
+PKGSRC_MODULES="rb/postgresql12-server rb/pgbouncer"
 
-CLEAN_MODULES="bmake bootstrap-mk-files bzip2 expat libidn2 perl libuv m4 libunistring automake autoconf bison bsdtar cmake cwrappers curl digest docbook-xsl docbook-xml fontconfig ghostscript-gpl ghostscript groff ghostscript-fonts freetype2 gettext-lib gettext-tools gmake gperf gtexinfo help2man jasper netpbm jbigkit tiff jpeg libarchive libICE libSM libXt libXaw libXmu libXpm libX11 libXext libXau libxcb libXdmcp libpaper libtool-base libxslt makedepend mandoc nbpatch p5-CPAN-Meta p5-Locale-libintl p5-Module-Build p5-Perl4-CoreLibs p5-Scalar-List-Utils p5-Text-Unidecode p5-Unicode-EastAsianWidth p5-gettext p5-inc-latest p5-Sub-Uplevel p5-Test-Exception p5-Test-Warn p5-Test-NoWarnings p5-Test-Simple pax pkgconf png py27-argparse py27-atomicwrites py27-test py27-attrs py27-cElementTree py27-xcbgen py27-funcsigs py27-linecache2 py27-unittest2 py27-pathlib2 py27-pbr py27-traceback2 py27-pluggy py27-py py27-scandir py27-setuptools_scm py27-setuptools_scm_git_archive rhash swig tradcpp xcb-proto xorgproto xtrans pkg_install"
+CLEAN_MODULES="autoconf automake bmake bootstrap-mk-files bsdtar bzip2 expat libidn2 perl libuv m4 libunistring bison bsdtar cmake cwrappers curl digest docbook-xsl docbook-xml fontconfig ghostscript-gpl ghostscript groff ghostscript-fonts freetype2 gettext-lib gettext-tools gmake gperf gtexinfo help2man jasper netpbm jbigkit tiff jpeg libarchive libICE libSM libXt libXaw libXmu libXpm libX11 libXext libXau libffi libxcb libXdmcp libpaper libuuid libtool-base libxslt makedepend mandoc nbpatch nghttp2 p5-CPAN-Meta p5-Locale-libintl p5-Module-Build p5-Perl4-CoreLibs p5-Scalar-List-Utils p5-Text-Unidecode p5-Unicode-EastAsianWidth p5-gettext p5-inc-latest p5-Sub-Uplevel p5-Test-Exception p5-Test-Warn p5-Test-NoWarnings p5-Test-Simple pax pkgconf png py27-argparse py27-atomicwrites py27-test py27-attrs py27-cElementTree py27-xcbgen py27-funcsigs py27-linecache2 py27-unittest2 py27-pathlib2 py27-pbr py27-traceback2 py27-pluggy py27-py py27-scandir py27-setuptools_scm py27-setuptools_scm_git_archive python37 rhash swig tradcpp xcb-proto xorgproto xtrans pkg_install"
 
 export PKGSRC_BASE
 export PREFIX
@@ -31,6 +31,7 @@ for module in $PKGSRC_MODULES
     _module=`echo $module | sed 's/.*\/\(.*\)/\1/g' | sed 's/py-/py\[0-9\]\{2\}-/g'`
     _nol=`$PREFIX/sbin/pkg_info |$GREP -P "^$_module" |wc -l`
     if [ $_nol -eq 0 ]; then
+      echo $module
       (cd ${PKGSRC_BASE}/pkgsrc/$module && bmake install clean clean-depends) || exit 1
     fi
   done
@@ -51,6 +52,7 @@ for module in $CLEAN_MODULES
 
 if [ "$_modules" != "" ]; then
   echo "Deleting modules: $_modules"
+#  $PREFIX/sbin/pkg_delete $_modules || exit 1
   $PREFIX/sbin/pkg_delete -ff $_modules || exit 1
 fi
 
