440729e9ea52a8db507b1268320699367e6ddcae
marceliq <mhnilka@gmail.com>
2021-08-24 08:05:28 +0000

    backup

---
+++ build-rsyslog.sh
@@ -0,0 +1,71 @@
+#!/usr/bin/env bash
+#set -x
+
+umask 022
+
+_CWD=`pwd`
+PKGSRC_BASE=/app
+PREFIX=/app/common/rsyslog
+CVS_BRANCH="HEAD"
+#PKGSRC_URL="https://cdn.netbsd.org/pub/pkgsrc/current/pkgsrc.tar.gz"
+
+PKGSRC_MODULES="sysutils/rsyslog"
+
+CLEAN_MODULES="asciidoc bmake bootstrap-mk-files bzip2 ccache expat flex libidn2 libuv m4 libunistring automake autoconf bison bsdtar cmake cwrappers curl db4 digest docbook-xsl docbook-xml fontconfig getopt ghostscript-gpl ghostscript groff ghostscript-fonts freetype2 gettext-lib gettext-tools gmake gperf gtexinfo help2man jasper netpbm jbigkit tiff jpeg libarchive libffi libgcrypt libgpg-error libuuid libxml2 libICE libSM libXt libXaw libXmu libXpm libX11 libXext libXau libxcb libXdmcp libpaper libtool-base libxslt makedepend mandoc nghttp2 nbpatch p5-CPAN-Meta p5-Locale-libintl p5-Module-Build p5-Perl4-CoreLibs p5-Scalar-List-Utils p5-Text-Unidecode p5-Unicode-EastAsianWidth p5-gettext p5-inc-latest p5-Sub-Uplevel p5-Test-Exception p5-Test-Warn p5-Test-NoWarnings p5-Test-Simple pax perl pkg_install pkgconf png python38 rhash swig tradcpp unzip xcb-proto xmlcatmgr xorgproto xtrans xz"
+
+# CLEAN_MODULES="asciidoc bzip2 ccache expat flex libidn2 libuv m4 libunistring automake autoconf bison bsdtar cmake cwrappers curl db4 digest docbook-xsl docbook-xml fontconfig getopt ghostscript-gpl ghostscript groff ghostscript-fonts freetype2 gettext-lib gettext-tools gmake gperf gtexinfo help2man jasper netpbm jbigkit tiff jpeg libarchive libffi libgcrypt libgpg-error libuuid libxml2 libICE libSM libXt libXaw libXmu libXpm libX11 libXext libXau libxcb libXdmcp libpaper libtool-base libxslt makedepend mandoc nghttp2 nbpatch p5-CPAN-Meta p5-Locale-libintl p5-Module-Build p5-Perl4-CoreLibs p5-Scalar-List-Utils p5-Text-Unidecode p5-Unicode-EastAsianWidth p5-gettext p5-inc-latest p5-Sub-Uplevel p5-Test-Exception p5-Test-Warn p5-Test-NoWarnings p5-Test-Simple pax perl pkgconf png python38 rhash swig tradcpp unzip xcb-proto xmlcatmgr xorgproto xtrans xz"
+
+export PKGSRC_BASE
+export PREFIX
+ 
+. base.sh
+
+# instalace pkgsrc modulu
+for module in $PKGSRC_MODULES
+  do
+    _module=`echo $module | sed 's/.*\/\(.*\)/\1/g' | sed 's/py-/py\[0-9\]\{2\}-/g'`
+    _nol=`$PREFIX/sbin/pkg_info |$GREP -P "^$_module" |wc -l`
+    if [ $_nol -eq 0 ]; then
+      (cd ${PKGSRC_BASE}/pkgsrc/$module && bmake install clean clean-depends) || exit 1
+    fi
+  done
+
+exit
+
+# cisteni prefixu
+_modules=""
+for module in $CLEAN_MODULES
+  do
+    _module=`echo $module | sed 's/py-/py\[0-9\]\{2\}-/g'`
+    _nol=`$PREFIX/sbin/pkg_info |$GREP -P "^$_module-[0-9]" |wc -l`
+#    echo $_module : $_nol
+    if [ $_nol -ne 0 ]; then
+      _modules="$_modules $_module"
+    fi
+  done
+
+if [ "$_modules" != "" ]; then
+  echo "Deleting modules: $_modules"
+#  $PREFIX/sbin/pkg_delete $_modules || exit 1
+  $PREFIX/sbin/pkg_delete -ff $_modules || exit 1
+fi
+
+rm -rf \
+$PREFIX/include \
+$PREFIX/conf \
+$PREFIX/bin/bmake \
+$PREFIX/info \
+$PREFIX/man \
+$PREFIX/pkgdb \
+$PREFIX/pkgdb.refcount \
+$PREFIX/share || exit 1
+
+for f in `find ${PREFIX} -type f | ${GREP} -P '\.pyc$|\.pyo$|\.a$|\.la$'`; do rm -f ${f}; done
+
+_actual=`pwd`
+(cd $PREFIX && python3 $_CWD/origin_rpath.py) || exit 1
+cd $_actual
+
+# vytvoreni balicku
+(cd $PREFIX/.. && tar czf nettools-`date +%Y%m%d`-`uname -s | tr '[:upper:]' '[:lower:]'`-`uname -p`.tar.gz nettools) || exit 1
+
---
+++ python38-base.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+. base.sh
+
+# doplneni promennych do mk.conf
+MKCONF_PATH=$PREFIX/conf/mk.conf
+
+props="PYTHON_DEFAULT_VERSION=\t38 PYTHON_VERSION_DEFAULT=\t38 PYPACKAGE=\t\tpython38 PKG_OPTIONS.python38=\t-x11"
+
+for prop in $props
+  do
+    _nol=`perl -nle "print if m/$prop/" $MKCONF_PATH | wc -l`
+    if [ $_nol -eq 0 ]; then
+      $SED "s/(\.endif.*)/$prop\n\1/g" $MKCONF_PATH || exit 1
+    fi
+  done
+
+# build a instalace
+# elftoolchain
+#_nol=`$PREFIX/sbin/pkg_info |$GREP elftoolchain |wc -l`
+#if [ $_nol -eq 0 ]; then
+#  (cd ${PKGSRC_BASE}/pkgsrc/devel/elftoolchain && bmake install clean clean-depends) || exit 1
+#fi
+
+# python
+_nol=`$PREFIX/sbin/pkg_info |$GREP python38 |wc -l`
+if [ $_nol -eq 0 ]; then
+  (cd ${PKGSRC_BASE}/pkgsrc/lang/python38 && bmake install clean clean-depends) || exit 1
+fi
+
